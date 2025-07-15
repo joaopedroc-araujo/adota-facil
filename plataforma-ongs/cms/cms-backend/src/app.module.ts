e8f7c6fc14ff7d8613f94f16120cad9d61e3046b
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-07-15 16:33:39 -0300

    experiment: add Nest folder and files structure

--- plataforma-ongs/cms/cms-backend/src/app.module.ts
+++ plataforma-ongs/cms/cms-backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediaModule } from './media/media.module';
+import { EventsModule } from './events/events.module';
+import { PagesModule } from './pages/pages.module';
+import { TenantsModule } from './tenants/tenants.module';
+import { UsersModule } from './users/users.module';
+import { AnimalsModule } from './animals/animals.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MediaModule,
+    EventsModule,
+    PagesModule,
+    TenantsModule,
+    UsersModule,
+    AnimalsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
