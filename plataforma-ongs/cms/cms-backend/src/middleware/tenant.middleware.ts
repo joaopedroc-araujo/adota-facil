1af4484b52a1348e01632bea26d24a016508b404
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-07-16 14:00:47 -0300

    experiment: feat: implement tenant middleware for subdomain-based tenant resolution

---
+++ plataforma-ongs/cms/cms-backend/src/middleware/tenant.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { TenantsService } from 'src/tenants/tenants.service';
+
+@Injectable()
+export class TenantMiddleware implements NestMiddleware {
+  constructor(private readonly tenantsService: TenantsService) {}
+
+  async use(req: Request, _res: Response, next: NextFunction) {
+    const host: string = req.headers.host?.split(':')[0] || '';
+    const parts = host.split('.');
+    const subdomain = parts.length > 2 ? parts[0] : '';
+
+    const skipDomains = ['localhost', '127.0.0.1', 'www', 'plataforma'];
+    if (!subdomain || skipDomains.includes(subdomain)) {
+      return next();
+    }
+
+    //TODO: create findBySubdomain method in tenantsService
+    // to fetch tenant by subdomain
+    const tenant = await this.tenantsService.findBySubdomain(subdomain);
+    if (!tenant) {
+      throw new NotFoundException('Tenant not found');
+    }
+
+    (req as any).tenantId = tenant.id;
+    (req as any).tenant = tenant;
+
+    return next();
+  }
+}
