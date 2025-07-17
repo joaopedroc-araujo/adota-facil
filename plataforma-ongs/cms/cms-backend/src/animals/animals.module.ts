f7dd631bb7efb6a286eaed58b23e1c6c906d534b
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-07-17 15:11:42 -0300

    experiment: fix: add repository as provider for animals module

--- plataforma-ongs/cms/cms-backend/src/animals/animals.module.ts
+++ plataforma-ongs/cms/cms-backend/src/animals/animals.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AnimalsService } from './animals.service';
 import { AnimalsController } from './animals.controller';
+import { AnimalsRepository } from './animals.repository';
 
 @Module({
   controllers: [AnimalsController],
-  providers: [AnimalsService],
+  providers: [
+    AnimalsService,
+    {
+      provide: 'IAnimalRepository',
+      useClass: AnimalsRepository,
+    },
+  ],
 })
 export class AnimalsModule {}
